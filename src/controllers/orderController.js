93263335a576787fc245df8171b58ecfd7a3daf3
Alexsandro-Almeida <alexalmeida67.093@gmail.com>
2026-03-09 19:44:35 -0300

    feat: implement PUT and DELETE methods

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -51,3 +51,27 @@ exports.listOrders = async (req, res) => {
         return res.status(500).json({ error: error.message });
     }
 };
+
+exports.updateOrder = async (req, res) => {
+    try {
+        const updatedOrder = await Order.findOneAndUpdate(
+            { orderId: req.params.orderId },
+            req.body,
+            { new: true }
+        );
+        if (!updatedOrder) return res.status(404).json({ message: "Pedido inexistente" });
+        return res.status(200).json(updatedOrder);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+};
+
+exports.deleteOrder = async (req, res) => {
+    try {
+        const deleted = await Order.findOneAndDelete({ orderId: req.params.orderId });
+        if (!deleted) return res.status(404).json({ message: "Pedido inexistente" });
+        return res.status(200).json({ message: "Pedido deletado com sucesso" });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+};
